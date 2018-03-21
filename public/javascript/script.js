e981a36b46ba6b0093acbfecee4e91ed8b390232
apard <apardo04@gmail.com>
2018-03-20 23:00:12 -0400

    adding donations popup on click; adding ltc qr code for donations; organizing html; css fixes

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -10,14 +10,15 @@ $(document).ready(function() {
     let change_1h = ltcData[0].percent_change_1h;
     let change_7d = ltcData[0].percent_change_7d;
 
-    let cap = ltcData[0].market_cap_usd.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,").slice(0, -2);
+    let cap = ltcData[0].market_cap_usd.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.").slice(0, -11);
 
     $('#usd_price').text("USD: $" + price)
     $('#change_7d').text(change_7d + "%")
     $('#change_24h').text(change_24h + "%")
     $('#change_1h').text(change_1h + "%")
     $('#btc_price').text("BTC: " + ltcData[0].price_btc)
-    $('#market_cap').text("Market Cap: " + cap + " ( Rank: " + ltcData[0].rank + " )")
+    $('#market_cap').text("Market Cap: " + cap + "B")
+    $('#rank').text(ltcData[0].rank)
 
     if (change_7d >= 0)
         $('#change_7d').css('color', '#40ba00')
@@ -26,12 +27,12 @@ $(document).ready(function() {
     if (change_1h >= 0)
         $('#change_1h').css('color', '#40ba00')
 
-    googleTrends.interestOverTime({keyword: 'Litecoin'})
-        .then(function(results){
-            console.log('These results are awesome', results);
-        })
-        .catch(function(err){
-            console.error('Oh no there was an error', err);
-        });
+    $('.donationButton').click(function() {
+        $('#donationsModal').show();
+    });
+
+    $('.closeDonations').click(function() {
+        $('#donationsModal').hide();
+    });
 });
 
